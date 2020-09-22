ccd66bf36a650811838e6c960583c40d8de08276
redHeadSet <stikfascube@gmail.com>
2020-09-22 09:05:41 +0900

    guessing_game 추가 (수정 코드 포함)

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,54 @@
+extern crate rand;
+
+use std::io;
+// use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    match guess.as_str().trim() {
+        "K" => println!("ooo"),
+        _   => println!("xxx")
+    }
+
+    loop {
+        println!("Please input your guess. A : {}", secret_number);
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(errmsg) => {
+                println!("{}", errmsg);
+                continue;
+            },
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess >= secret_number {
+            true  => println!("true "),
+            false => println!("false")
+        }
+
+        // match guess.cmp(&secret_number) {
+        //     Ordering::Less    => println!("Too small!"),
+        //     Ordering::Greater => println!("Too big!"),
+        //     Ordering::Equal   => {
+        //         println!("You win!");
+        //         break;
+        //     }
+        // }
+    }
+}
