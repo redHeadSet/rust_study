20b370a3240cbaf71bff7eee7a03245524cbc735
MinheeJu <minhee7219@gmail.com>
2020-09-23 20:52:13 +0900

    진정하지 못한 println!

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -2,6 +2,20 @@ fn main() {
     println!("Hello, world!");
 
     println!("vcsc gukhum");
-
     println!("hello, Minhee modify");
+    println!("hello, Minhee modify");
+    println!("hello, Minhee modify");
+    println!("hello, Minhee modify");
+    println!("hello, Minhee modify");
+    println!("hello, Minhee modify");
+    println!("hello, Minhee modify");
+    println!("hello, Minhee modify");
+    println!("hello, Minhee modify");
+    println!("hello, Minhee modify");
+    println!("hello, Minhee modify");
+    println!("hello, Minhee modify");
+    println!("hello, Minhee modify");
+    println!("hello, Minhee modify");
+    println!("hello, Minhee modify");
+
 }
