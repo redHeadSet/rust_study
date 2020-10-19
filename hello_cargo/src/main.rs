cb457e6e116f73e4679dd4952d0f5cc6bd0ed175
MinheeJu <minhee7219@gmail.com>
2020-10-19 22:44:35 +0900

    10-14 진행 : 구조체 및 메소드 문법

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -37,7 +37,52 @@ fn main() {
   
 //   let slice = s[0 .. 3];  // str the size for values of type `str` cannot be known at compilation time
 //   println!("1. {}",String::from(&slice));
+
+  struct User {
+        username: String,
+        email: String,
+        sign_in_count: u64,
+        active: bool,
+  }
+
+  let  user1 = User {
+    email: String::from("someone@example.com"),
+    username: String::from("someusername123"),
+    active: true,
+    sign_in_count: 1,
+  };
+//   user1.email = String::from("qweqweqwe");
+//   print!("{}",user1.email);
+
+//   fn build_user(email: String, username: String) -> User {
+//     User {
+//         email,
+//         username,
+//         active: true,
+//         sign_in_count: 1,
+//     };
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        ..user1
+    };
+
+    print!("{}", user2.email);
+    print!("{}", user2.username);
+    print!("{}", user2.active);
+    print!("{}", user2.sign_in_count);
+
+
+    
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+let black = Color(0, 0, 0);
+let origin = Point(0, 0, 0);
+
 }
+
 
 fn calculate_length(s: &String) -> usize {
     s.len()
